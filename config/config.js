9069ae25140baa3b69eee91a933d136b61bf74ab
joaccocaballero <joaquincaballero182@gmail..com>
2024-03-25 17:57:27 -0300

    camio

--- config/config.js
+++ config/config.js
@@ -8,4 +8,17 @@ module.exports = {
     "host": process.env.DEV_PG_HOST,
     "dialect": "postgres"
   },
+  "production": {
+    "username": process.env.DEV_PG_USERNAME,
+    "password": process.env.DEV_PG_PASSWORD,
+    "database": process.env.DEV_PG_DATABASE,
+    "host": process.env.DEV_PG_HOST,
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        require: true,
+        rejectUnauthorized: false
+	}
+    }
+  }
 }
