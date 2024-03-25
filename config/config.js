1d99904d748537f37e01612863356127478ecb40
joaccocaballero <joaquincaballero182@gmail..com>
2024-03-25 18:03:20 -0300

    cambios config production

--- config/config.js
+++ config/config.js
@@ -18,20 +18,7 @@ module.exports = {
       "ssl": {
         require: true,
         rejectUnauthorized: false
-	}
-    }
-  }
-  "production": {
-    "username": process.env.DEV_PG_USERNAME,
-    "password": process.env.DEV_PG_PASSWORD,
-    "database": process.env.DEV_PG_DATABASE,
-    "host": process.env.DEV_PG_HOST,
-    "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": {
-        require: true,
-        rejectUnauthorized: false
-	}
+	    }
     }
   }
 }
